22c357b61cb1878ced791f9497ccd0da8aede8db
Victor <victor@localhost.localdomain>
2017-04-07 23:54:19 -0300

    create function print_graph() and improved function build_graph()

--- MathFunctions.py
+++ MathFunctions.py
@@ -1,9 +1,11 @@
-#class MathFunctions():
+import random
 
-def mmc(numbers):
+def lcm(numbers):
+	"Return the least commom multiple of numbers's elements"
+
 	i = 2
 	length = len(numbers)
-	mmc = 1
+	lcm = 1
 	while (sum(numbers) != len(numbers)):
 		usefull = False
 		for j in range(length):
@@ -12,14 +14,16 @@ def mmc(numbers):
 				usefull=True
 
 		if (usefull):
-			mmc*=i
+			lcm*=i
 		else:
 			i+=1
 
-	return mmc
+	return lcm
 
-def mdc(numbers):
-	i, mdc = 2, 1
+def gcf(numbers):
+	"Return the greatest commom factor of numbers's elements"
+
+	i, gcf = 2, 1
 	length = len(numbers)
 	while (sum(numbers) != len(numbers)):
 		usefull = False
@@ -33,8 +37,67 @@ def mdc(numbers):
 		if (not usefull):
 			i+=1
 		if (k == length):
-			mdc*=i
+			gcf*=i
 
-	return mdc
+	return gcf
+
+def build_graph(nodes_number, maw=1, oriented=False):
+	'''Returns a random graph with 'nodes_number' nodes, represented by a adjacency matrix
+	maw: max arc weight'''
+
+	graph = [[0 for j in range(nodes_number)] for i in range(nodes_number)]
+
+	if oriented:
+		for i in range(nodes_number):
+			for j in range(nodes_number):
+				a = random.randint(0, maw)
+				graph[i][j] = a
+			graph[i][i] = 0
+		return graph	
+
+	else:
+		for i in range(nodes_number):
+			for j in range(i, nodes_number):
+				a = random.randint(0, maw)
+				graph[i][j] = a
+				graph[j][i] = a
+
+			graph[i][i] = 0
+
+		return graph
+
+def print_graph(simple_graph):
+	"Print a simple graph represented by a adjacency matrix"
+	num = 1
+	for n in simple_graph:
+		print(str(num) + ' ' + str(n))
+		num += 1
+
+
+class Node():
+	'''Node of a graph. Weight is a number, 
+	state can be anything, but could be usefull like a string'''
+
+	def __init__(self, weight=1, state=None):
+		self.weight = weight
+
+
+class Arc():
+	"Arc of a graph. Tail and head are nodes"
+
+	def __init__(self, tail, head, weight=1):
+		self.tail = tail
+		self.head = head
+		self.weight = weight
+
+
+
+class Simple_Graph():
+
+	def __init__(self, nodes, arcs):
+		self.nodes = nodes
+		self.arcs = arcs
+
+
 
 
